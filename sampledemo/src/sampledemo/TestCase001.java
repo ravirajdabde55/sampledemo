67bb848e6420fb71d3d0c8a26d4b12edd209714c
LENOVO <LENOVO@DESKTOP-CEPDD2L>
2023-12-09 00:23:50 +0530

    first commit

---
+++ sampledemo/src/sampledemo/TestCase001.java
@@ -0,0 +1,11 @@
+package sampledemo;
+
+public class TestCase001 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is the first test case for regreestion testing...");
+
+	}
+
+}
